6718497aa6cc57137c8f6ff8aa43f9d98f89b00a
TEJAS-SANKPAL03 <sankpaltejas400@gmail.com>
2026-08-18 22:58:13 +0530

    first commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello Developer's 👋</Text>
+      <Text style={styles.subtitle}>Welcome to Dummy app </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    marginTop: 10,
+    fontSize: 16,
+    color: '#666',
+  },
+});
